446be7b264bd910409563ee2b10a36881bb330c2
Chan Woo <14cpark@gmail.com>
2020-05-09 12:06:01 -0700

    Forward employee logins to dashboard page

--- WebContent/loginemployee.js
+++ WebContent/loginemployee.js
@@ -11,7 +11,7 @@ function handleLoginResult(resultDataString) {
 
 	// If login succeeds, it will redirect the user to mainpage.html
 	if (resultDataJson["status"] === "success") {
-		window.location.replace("mainpage.html");
+		window.location.replace("dashboard.html");
 	} else {
 		// If login fails, the web page will display
 		// error messages on <div> with id "login_error_message"
