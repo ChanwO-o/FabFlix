41d705da8c31b33f180918ef8e2d5369d4960e2b
Sung Soo Kim <sungsok@uci.edu>
2020-05-09 20:11:54 -0700

    Call add_movie stored procedure

--- src/DashboardServlet.java
+++ src/DashboardServlet.java
@@ -24,42 +24,70 @@ public class DashboardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 //		System.out.println(request.getRequestURI());
 
+		// for add star
 		String name = request.getParameter("name");
 		String birthyear = request.getParameter("birthyear");
+		// for add movie
 		String title = request.getParameter("title");
 		String director = request.getParameter("director");
 		String year = request.getParameter("year");
 		String genre = request.getParameter("genre");
 		String star_name = request.getParameter("star_name");
 
-		System.out.println("DashboardServlet doGet() params name: " + name + " birthyear: " + birthyear);
+		System.out.println("DashboardServlet doGet() params name: " + name + " birthyear: " + birthyear + "title: " + title + " director: " + director +
+				" year: " + year + " genre: " + genre + " star_name: " + star_name);
 
-		if (name == null) {
-			if(title ==null || director==null || year==null || genre==null || star_name==null)
+		if (name == null) { // add movie
+			if(title == null || director == null || year == null || genre == null || star_name == null)
 				return;
-			else {
-				try{
-					Connection dbcon = dataSource.getConnection();
-					dbcon.setAutoCommit(false);
-					String query = "SELECT id FROM movies ORDER BY id DESC LIMIT 1;";
-					Statement statement = dbcon.createStatement();
-					ResultSet rs = statement.executeQuery(query);
-					dbcon.commit();
-					rs.next();
-					String lastMovieId = rs.getString("id");
-					System.out.println("last movie id: " + lastMovieId);
+			try{
+				Connection dbcon = dataSource.getConnection();
+				dbcon.setAutoCommit(false);
+				String query = "SELECT id FROM movies ORDER BY id DESC LIMIT 1;";
+				Statement statement = dbcon.createStatement();
+				ResultSet rs = statement.executeQuery(query);
+				dbcon.commit();
+				rs.next();
+				String lastMovieId = rs.getString("id");
+				System.out.println("last movie id: " + lastMovieId);
 
-					String newMovieId = "tt" + Integer.parseInt(lastMovieId.substring(2)) + 1;
+				String newMovieId = "tt" + Integer.parseInt(lastMovieId.substring(2)) + 1;
+
+				// now get star id (existing/new)
+				String getLastStarQuery = "SELECT id FROM stars ORDER BY id DESC LIMIT 1;"; // get last id number from stars
+				Statement getLastStarStatement = dbcon.createStatement();
+				ResultSet rs2 = getLastStarStatement.executeQuery(getLastStarQuery);
+				dbcon.commit();
+				rs2.next();
+				String lastStarId = rs2.getString("id");
+				System.out.println("last star id found: " + lastStarId);
+				String newId = "nm" + (Integer.parseInt(lastStarId.substring(2)) + 1); // get integer portion of id + 1
+				System.out.println("new star id: " + newId);
 
 
+				CallableStatement cstatement = dbcon.prepareCall("{call add_movie(?,?,?,?,?,?,?)}");
+				cstatement.setString(1, newMovieId);
+				cstatement.setString(2, title);
+				cstatement.setInt(3, Integer.parseInt(year));
+				cstatement.setString(4, director);
+				cstatement.setString(5, star_name);
+				cstatement.setString(6, newId);
+				cstatement.setString(7, genre);
+				System.out.println(cstatement);
+				ResultSet rs3 = cstatement.executeQuery();
+				dbcon.commit();
+				rs3.next();
+				rs.close();
+				rs3.close();
+				dbcon.close();
 
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
+
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
 			}
 		}
-		else {
+		else { // add star
 			try {
 				Connection dbcon = dataSource.getConnection();
 				dbcon.setAutoCommit(false);
